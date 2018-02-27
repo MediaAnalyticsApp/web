45f5284572bcf38cdfba3139473ca9437e585ae0
v-olga-v <ol4ik_22@mail.ru>
2018-02-27 18:16:15 +0600

    add test in adminapp

--- adminapp/tests.py
+++ adminapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from adminapp.models import Persons
+
+
+class AuthorModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Persons.objects.create(name='Vladimir Putin')
+
+    def test_name_label(self):
+        persons = Persons.objects.get(id=1)
+        field_label = persons._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_name_max_length(self):
+        persons = Persons.objects.get(id=1)
+        max_length = persons._meta.get_field('name').max_length
+        self.assertEquals(max_length, 255)
